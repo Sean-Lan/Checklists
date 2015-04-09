2ef5be64566bbd0cfd660b7cca565629e1866be1
Sean-Lan <sean.lan.thu@gmail.com>
2015-04-09 20:57:44 +0800

    Add contents for row and the handler for cell selection.

--- Checklists/ChecklistsViewController.m
+++ Checklists/ChecklistsViewController.m
@@ -25,11 +25,33 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 5;
+    return 100;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+    UILabel *label = (UILabel *)[cell viewWithTag:1000];
+    if (indexPath.row%5 == 0) {
+        label.text = @"我是蓝翔第一行";
+    } else if (indexPath.row%5 == 1) {
+        label.text = @"我是蓝翔第二行";
+    } else if (indexPath.row%5 == 2) {
+        label.text = @"我是蓝翔第三行";
+    } else if (indexPath.row%5 == 3) {
+        label.text = @"我是蓝翔第四行";
+    } else if (indexPath.row%5 == 4) {
+        label.text = @"我是蓝翔第五行";
+    }
     return cell;
+}
+
+- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath*)indexPath {
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    if (cell.accessoryType == UITableViewCellAccessoryNone) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 @end
